46ccfe2e705ea8479d7654f7c1044d1e509f1e4d
vyzo <vyzo@hackzen.org>
2021-04-08 18:11:57 +0300

    add tests

---
+++ resolver_test.go
@@ -0,0 +1,30 @@
+package doh
+
+import (
+	"context"
+	"testing"
+)
+
+func TestLookupIPAddr(t *testing.T) {
+	r := NewResolver("https://cloudflare-dns.com/dns-query")
+
+	ips, err := r.LookupIPAddr(context.Background(), "ewr-1.bootstrap.libp2p.io")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(ips) == 0 {
+		t.Fatal("got no IPs")
+	}
+}
+
+func TestLookupTXT(t *testing.T) {
+	r := NewResolver("https://cloudflare-dns.com/dns-query")
+
+	txt, err := r.LookupTXT(context.Background(), "_dnsaddr.bootstrap.libp2p.io")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(txt) == 0 {
+		t.Fatal("got no TXT entries")
+	}
+}
